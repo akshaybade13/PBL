2eb66a1a1a0e207617bd1e898812a3775969b06b
Mission-SEPBL <139857947+Mission-SEPBL@users.noreply.github.com>
2023-07-18 14:18:07 +0530

    Update userDetails.js

--- userDetails.js
+++ userDetails.js
@@ -9,7 +9,7 @@ const passportLocalMongoose = require('passport-local-mongoose');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect("mongodb://0.0.0.0:27017/ProjectDB");
+mongoose.connect("mongodb+srv://missionsepbl:Outoffinpbl09@cluster0.dcap7k5.mongodb.net/");
 const User = new mongoose.Schema({
     username: String,
     password: String,
